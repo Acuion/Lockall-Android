9070784211aace9593ef8f7bcd6b6ea0f3931ee5
natarur <natarur@LENOVO-THINK>
2018-04-12 11:29:37 +0300

    qr parsing

--- app/src/main/java/me/acuion/lockall_android/MainActivity.kt
+++ app/src/main/java/me/acuion/lockall_android/MainActivity.kt
@@ -8,6 +8,8 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.FileInputStream
 import java.net.Inet4Address
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 import java.nio.charset.Charset
 import java.security.KeyStore
 
@@ -18,19 +20,36 @@ class MainActivity : Activity() {
             42 -> {
                 runOnUiThread {
                     val base64FromQr = data!!.extras.getString("data")
-                    val qrBytes : ByteArray
+                    val qrBytes : ByteBuffer
                     try {
-                        qrBytes = Base64.decode(base64FromQr, 0)
+                        qrBytes = ByteBuffer.wrap(Base64.decode(base64FromQr, 0))
                     } catch (ex : IllegalArgumentException) {
                         Toast.makeText(applicationContext, "Base64 decode failed", Toast.LENGTH_LONG).show()
                         return@runOnUiThread
                     }
-
+                    qrBytes.order(ByteOrder.LITTLE_ENDIAN)
                     // 4len + fc
                     // 4len + sc
                     // 4ip
                     // 4port
                     // {rest} - machine+username
+                    var fcLen = qrBytes.getInt()
+                    val firstComponent = ByteArray(fcLen) //
+                    qrBytes.get(firstComponent)
+                    val scLen = qrBytes.getInt()
+                    val secondComponent = ByteArray(scLen) //
+                    qrBytes.get(secondComponent)
+                    val ipBuffer = ByteArray(4)
+                    qrBytes.get(ipBuffer)
+                    val localPort = qrBytes.getInt() //
+                    val hostNameBuffer = ByteArray(qrBytes.remaining())
+                    qrBytes.get(hostNameBuffer)
+
+                    val localIp = Inet4Address.getByAddress(ipBuffer) //
+                    val hostName = String(hostNameBuffer, Charset.forName("UTF-8")) //
+
+                    Toast.makeText(applicationContext,
+                            "$firstComponent and $secondComponent from $hostName at $localIp:$localPort", Toast.LENGTH_LONG).show()
                 }
             }
         }
